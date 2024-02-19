da48fa21612359307bca3282fef3023c7c77a762
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-02-19 13:32:58 -0600

    List of Exercises
    creating exercises to choose from

---
+++ exerciseRoutines.js
@@ -0,0 +1,6 @@
+//creating exercises to choose from
+const exerciseRoutines = {
+    strength: ["Push-ups", "Pull-ups", "Squats", "Deadlifts"],
+    cardio: ["Running", "Cycling", "Jumping Jacks", "Burpees"],
+    flexibility: ["Yoga", "Stretching", "Pilates", "Tai Chi"]
+  };
