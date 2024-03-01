f1008ce3675926030937e21eb2107960fd1e81ba
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-02-29 20:21:43 -0600

    Create trackProgress.js

---
+++ trackProgress.js
@@ -0,0 +1,8 @@
+
+  function trackProgress() {
+    const caloriesConsumed = document.getElementById("caloriesConsumed").value;
+    const caloriesBurned = document.getElementById("caloriesBurned").value;
+    // A function will be used to handle tracking progress in the database
+    // trackProgressInDatabase(caloriesConsumed, caloriesBurned);
+    alert("Progress tracked successfully!");
+  }
