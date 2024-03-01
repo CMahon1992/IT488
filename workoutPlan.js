cb4b1aae3b7206114cd017e598293396cce19981
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-02-29 20:22:12 -0600

    Create workoutPlan.js

---
+++ workoutPlan.js
@@ -0,0 +1,7 @@
+function saveWorkoutPlan() {
+    const workoutName = document.getElementById("workoutName").value;
+    const exercises = document.getElementById("exercises").value;
+    // function will be used save workout plans to the database
+    // saveWorkoutPlanToDatabase(workoutName, exercises);
+    alert("Workout plan saved successfully!");
+  }
